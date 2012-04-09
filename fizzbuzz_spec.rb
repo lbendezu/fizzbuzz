39ebcd6e0041e2c8aceccf9d05bcf95a3c3efda8
Luis Bendezu <luisbendezuloayza@gmail.com>
2012-04-08 22:54:31 -0500

    Primer test, se cae en el primer test porque la clase no existe.

--- fizzbuzz_spec.rb
+++ fizzbuzz_spec.rb
@@ -1,5 +1,13 @@
-require "./fizzbuzz.rb"
+require "./FizzBuzz.rb"
 
-describe fizzbuzz do
+describe FizzBuzz do
+
+	before(:each) do
+		@fizzbuzz = FizzBuzz.new
+	end
+
+	it "Si es multiplo de 3 debe imprimir Fizz" do
+  	@fizzbuzz.imprimir_numero(3).should == "Fizz"
+	end
 
 end
